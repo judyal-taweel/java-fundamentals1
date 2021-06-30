021e8b167f286f86de53fe8fe7bdc59f76b53b8a
judyal-taweel <judybataineh@gmail.com>
2021-06-30 20:31:48 +0300

    coding for lab02

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,7 @@
 package com.company;
 
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -49,50 +46,40 @@ public class Main {
         int avg= sum/(arr.length);
 
         return avg;
-    }
+}
 
-//    public static int dimensionalArr(int[][] arr) {
-//        int sum=0;
-//        int avg=0;
-//        for (int i = 0; i< arr[0].length; i++) {
-//            sum += i;
-//        }
-//        avg=sum/arr[0].length;
-//
-//        for(int j=0 ;j<arr.length;j++)
-//        {
-//            sum=0;
-//
-//        }
-//    }
+     public static void arrayOfArray()
+     {
+         int[][] weeklyMonthTemperatures = {
+                 {66, 64, 58, 65, 71, 57, 60},
+                 {57, 65, 65, 70, 72, 65, 51},
+                 {55, 54, 60, 53, 59, 57, 61},
+                 {65, 56, 55, 52, 55, 62, 57}
+         };
 
-
-//    public static String pluralize (String word , int num)
-//    {
-//        if(num == 1){
-//            return word;
-//        }else{
-//            return word+"s";
-//        }
-//}
-//
-//    public static void clock()
-//    {
-//        LocalDateTime now = LocalDateTime.now();
-//
-//            int hour = now.getHour();
-//            int minute = now.getMinute();
-//            int second = now.getSecond();
-//            String time = now.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
-//            System.out.println(time);
-//
-//    }
+         int minimum=0;
+         int sum;
+         int avg;
+         for(int i=0; i<weeklyMonthTemperatures.length; i++){
+             sum=0;
+             for(int j=0; j<weeklyMonthTemperatures[i].length;j++){
+                 sum+=weeklyMonthTemperatures[i][j];
+             }
+             avg = sum/weeklyMonthTemperatures[i].length;
+             minimum=sum/weeklyMonthTemperatures[0].length;
+             if(minimum >avg){
+                 minimum=avg;
+             }
+         }
+         System.out.println(minimum);
+     }
 
 
-        public static void main (String[]args)
-//                throws InterruptedException
-        {
-            roll(6);
+    public static void main(String[] args) throws InterruptedException
+    {
+
+        arrayOfArray();
+           roll(6);
             int[] arr={2,5,3,8,6};
            System.out.println(containsDuplicates(arr,3));
 
@@ -100,35 +87,12 @@ public class Main {
            System.out.println(avgCal(array));
 
 
-            int[][] weeklyMonthTemperatures = {
-                    {66, 64, 58, 65, 71, 57, 60},
-                    {57, 65, 65, 70, 72, 65, 51},
-                    {55, 54, 60, 53, 59, 57, 61},
-                    {65, 56, 55, 52, 55, 62, 57}
-            };
 
 
 
-//        int dogCount = 1;
-//        System.out.println("I own " + dogCount + " " + pluralize("dog", dogCount) + ".");
-//
-//        int catCount = 2;
-//        System.out.println("I own " + catCount + " " + pluralize("cat", catCount) + ".");
-//
-//        int turtleCount = 0;
-//        System.out.println("I own " + turtleCount + " " + pluralize("turtle", turtleCount) + ".");
-//
-//
-//
-//        while (true) {
-//            clock();
-//            for (int i = 0; i < 15; i++) {
-//                Thread.sleep(60);
-//            }
-//
-//
-//        }
-//
 
-        }
+
+
+
     }
+}
